c1a6b8a21e3eb4973dff44d07e3501e91d8ea526
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-21 23:08:52 -0500

    Downloads response and writes file

--- fetcher.js
+++ fetcher.js
@@ -6,18 +6,39 @@
 
 // It should download the resource at the URL to the local path on your machine. Upon completion, it should print out a message like Downloaded and saved 1235 bytes to ./index.html.
 
-const needle = require('needle');
+// Asynchronous Operations
+// There are two operations in this problem which will take an unknown amount of time:
 
+// You need to make an http request and wait for the response.
+// After the http request is complete, you need to take the data you receive and write it to a file in your local filesystem.
+// ------------------------------------------------------------------------
+
+
+const needle = require('needle');
+const fs = require('fs'); // for writing files 
+
+
+// taking arguments from the command line
 const args = process.argv;
 const url = args[2];
 const filePath = args[3]; 
 
+// creating the connection using needle library
 needle.get(url, (error, response) => {
   if (error) {
     console.error('Error:', error);
   } else {
-    console.log('Status Code:', response.statusCode);
-    console.log('Response Headers:', response.headers);
-    console.log('Body:', response.body); // Prints the content of the website
+    // assuming theres is no error, so writing the file
+    fs.writeFile('./index.html', response.body, err => {
+      if (err) {
+        console.error("Error writing file:", err);
+      } else {
+        const fileSize = response.body.length;
+        console.log(`Downloaded and saved.`);
+      }
+    });
   }
 });
+
+
+
